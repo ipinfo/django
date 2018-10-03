523816e663cd26d8975f728bee1418351e829abe
James Timmins <jameshtimmins@gmail.com>
2018-10-03 13:49:54 -0700

    Version update.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 long_description = 'The official Django library for IPinfo. IPinfo prides itself on being the most reliable, accurate, and in-depth source of IP address data available anywhere. We process terabytes of data to produce our custom IP geolocation, company, carrier and IP type data sets. You can visit our developer docs at https://ipinfo.io/developers.'
 
 setup(name='ipinfo_django',
-      version='0.1.3',
+      version='0.1.4',
       description='Official Django library for IPinfo',
       long_description=long_description,
       url='https://github.com/ipinfo/django',
