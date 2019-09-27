31087870ac603c1c842e15bd7f77face459b2037
Uman Shahzad <uman@mslm.me>
2019-09-27 14:36:23 +0500

    Log any tracebacks during request processing and continue gracefully.

--- ipinfo_django/middleware.py
+++ ipinfo_django/middleware.py
@@ -1,27 +1,35 @@
+import logging
+import traceback
+
 from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 import ipinfo
 
+LOGGER = logging.getLogger(__name__)
+
 
 class IPinfo(MiddlewareMixin):
-
     def __init__(self, get_response=None):
         """Initializes class while gettings user settings and creating the cache."""
         self.get_response = get_response
-        self.filter = getattr(settings, 'IPINFO_FILTER', self.is_bot)
+        self.filter = getattr(settings, "IPINFO_FILTER", self.is_bot)
 
-        ipinfo_token = getattr(settings, 'IPINFO_TOKEN', None)
-        ipinfo_settings = getattr(settings, 'IPINFO_SETTINGS', {})
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
         self.ipinfo = ipinfo.getHandler(ipinfo_token, **ipinfo_settings)
 
     def process_request(self, request):
         """Middleware hook that acts on and modifies request object."""
-        if self.filter and self.filter(request):
+        try:
+            if self.filter and self.filter(request):
+                request.ipinfo = None
+            else:
+                request.ipinfo = self.ipinfo.getDetails()
+        except Exception as exc:
             request.ipinfo = None
-        else:
-            request.ipinfo = self.ipinfo.getDetails()
+            LOGGER.error(traceback.format_exc())
 
     def is_bot(self, request):
         """Whether or not the request user-agent self-identifies as a bot"""
-        lowercase_user_agent = request.META.get('HTTP_USER_AGENT', '').lower()
-        return 'bot' in lowercase_user_agent or 'spider' in lowercase_user_agent
+        lowercase_user_agent = request.META.get("HTTP_USER_AGENT", "").lower()
+        return "bot" in lowercase_user_agent or "spider" in lowercase_user_agent
--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup
 
-long_description = 'The official Django library for IPinfo. IPinfo prides itself on being the most reliable, accurate, and in-depth source of IP address data available anywhere. We process terabytes of data to produce our custom IP geolocation, company, carrier and IP type data sets. You can visit our developer docs at https://ipinfo.io/developers.'
+long_description = """
+The official Django library for IPinfo.
 
-setup(name='ipinfo_django',
-      version='0.1.4',
-      description='Official Django library for IPinfo',
-      long_description=long_description,
-      url='https://github.com/ipinfo/django',
-      author='James Timmins',
-      author_email='jameshtimmins@gmail.com',
-      license='Apache License 2.0',
-      packages=['ipinfo_django'],
-      install_requires=[
-        'django',
-        'ipinfo',
-      ],
-      zip_safe=False)
+IPinfo prides itself on being the most reliable, accurate, and in-depth source of IP address data available anywhere.
+We process terabytes of data to produce our custom IP geolocation, company, carrier and IP type data sets.
+You can visit our developer docs at https://ipinfo.io/developers.
+"""
+
+setup(
+    name="ipinfo_django",
+    version="1.0.0",
+    description="Official Django library for IPinfo",
+    long_description=long_description,
+    url="https://github.com/ipinfo/django",
+    author="IPinfo",
+    author_email="support@ipinfo.io",
+    license="Apache License 2.0",
+    packages=["ipinfo_django"],
+    install_requires=["django", "ipinfo==2.0.0"],
+    zip_safe=False,
+)
